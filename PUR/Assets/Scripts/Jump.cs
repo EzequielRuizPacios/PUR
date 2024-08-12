00937fb22d5803e7460222bf1b5238af1c2c5db9
Ezequiel Ruiz PAcios <Restiur1@gmail.com>
2024-08-12 13:29:02 -0300

    Prototipo base
    Prototipo base para poder trabajar en el juego

---
+++ PUR/Assets/Scripts/Jump.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    private Rigidbody2D myRb2d;
+    private CircleCollider2D sidewalkChecker;
+
+
+    private float jumpForce = 300f;
+    private bool canJump = true;
+
+    private void Awake()
+    {
+        myRb2d = GetComponent<Rigidbody2D>();
+        sidewalkChecker = GetComponent<CircleCollider2D>();
+    }
+    public void JumpCommand()
+    {
+        if (canJump)
+        {
+            myRb2d.AddForce(new Vector2(0f, jumpForce));
+            canJump = false;
+        }
+    }
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.transform.CompareTag("Sidewalk"))
+            {
+                canJump = true;
+            }
+        }
+    }
