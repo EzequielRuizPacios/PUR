7e1a807ad6dc7c5f9d8b7caf680428199b920360
Ezequiel Ruiz PAcios <Restiur1@gmail.com>
2024-08-16 20:13:58 -0300

    dsadsa
    asddsa

--- PUR/Assets/Scripts/GameManager.cs
+++ PUR/Assets/Scripts/GameManager.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    // [SerializeField] private Player player;
     public static GameManager Instance { get; private set; }
     
     
