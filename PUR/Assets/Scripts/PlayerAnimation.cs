7d9b6e7c69643147d29d0f539a6ced173cd8d51a
Restiur1 <restiur1@gmail.com>
2024-08-27 16:36:57 -0300

    dash
    Add: Dash button
    Add: Dash animation
    Fixed: Some inconsistency in few prefabs

--- PUR/Assets/Scripts/PlayerAnimation.cs
+++ PUR/Assets/Scripts/PlayerAnimation.cs
@@ -5,42 +5,45 @@ using UnityEngine.Playables;
 
 public class PlayerAnimation : MonoBehaviour
 {
-    private Player player;
-    private Animator playerAnimator;
+  private Player player;
+  private Animator playerAnimator;
 
-    private void OnEnable()
+  private void OnEnable()
+  {
+    player.onPlayerStateChanged += SetPlayerAnimation;
+  }
+
+  private void OnDisable()
+  {
+    player.onPlayerStateChanged -= SetPlayerAnimation;
+  }
+
+  private void Awake()
+  {
+    player = GameObject.Find("Player").GetComponent<Player>();
+    playerAnimator = GameObject.Find("Player").GetComponent<Animator>();
+  }
+
+  private void SetPlayerAnimation(Player.PlayerState newState)
+  {
+    if (newState == Player.PlayerState.Running)
     {
-        player.onPlayerStateChanged += SetPlayerAnimation;
+      playerAnimator.Play("run");
     }
 
-    private void OnDisable()
+    if (newState == Player.PlayerState.ShortJumping)
     {
-        player.onPlayerStateChanged -= SetPlayerAnimation;
+      playerAnimator.Play("shortJump");
     }
 
-    private void Awake()
+    if (newState == Player.PlayerState.LongJumping)
     {
-        player = GameObject.Find("Player").GetComponent<Player>();
-        playerAnimator = GameObject.Find("Player").GetComponent<Animator>();
-    }  
-
-    private void SetPlayerAnimation(Player.PlayerState newState)
-    {
-        if (player.State == Player.PlayerState.Running)
-        {
-            playerAnimator.Play("run");
-        }
-
-        if (player.State == Player.PlayerState.ShortJumping)
-        {
-            playerAnimator.Play("shortJump");
-        }
-
-        if (player.State == Player.PlayerState.LongJumping)
-        {
-            playerAnimator.Play("longJump");
-        }
-
-        Debug.Log(player.State);
+      playerAnimator.Play("longJump");
     }
+
+    if (newState == Player.PlayerState.Dashing)
+    {
+      playerAnimator.Play("dash");
+    }
+  }
 }
--- PUR/Assets/Scripts/PlayerCommands.cs
+++ PUR/Assets/Scripts/PlayerCommands.cs
@@ -4,47 +4,64 @@ using UnityEngine;
 
 public class PlayerCommands : MonoBehaviour
 {
-    // References
-    private Rigidbody2D myRb2d;
-    private CircleCollider2D sidewalkChecker;
-    private Player player;
+  // References
+  [SerializeField] private BoxCollider2D boxCollider2d_top;
+  private Rigidbody2D myRb2d;
+  private CircleCollider2D sidewalkChecker;
+  private Player player;
 
 
-    private float shortJumpForce = 300f;
-    private float longJumpForce = 450f;
-    private bool canJump = true;
 
-    private void Awake()
-    {
-        myRb2d = GetComponent<Rigidbody2D>();
-        sidewalkChecker = GetComponent<CircleCollider2D>();
-        player = GameObject.Find("Player").GetComponent<Player>();
-    }
-    public void ShortJumpCommand()
-    {
-        if (canJump && !GameManager.Instance.IsGamePaused)
-        {
-            myRb2d.AddForce(new Vector2(0f, shortJumpForce));
-            canJump = false;
-            player.SetPlayerState(Player.PlayerState.ShortJumping);
-        }
-    }
+  private float shortJumpForce = 300f;
+  private float longJumpForce = 450f;
+  private bool isGrounded = true;
 
-    public void LongJumpCommand()
+  private void Awake()
+  {
+    myRb2d = GetComponent<Rigidbody2D>();
+    sidewalkChecker = GetComponent<CircleCollider2D>();
+    player = GameObject.Find("Player").GetComponent<Player>();
+  }
+  public void ShortJumpCommand()
+  {
+    if (isGrounded && !GameManager.Instance.IsGamePaused)
     {
-        if (canJump && !GameManager.Instance.IsGamePaused)
-        {
-            myRb2d.AddForce(new Vector2(0f, longJumpForce));
-            canJump = false;
-            player.SetPlayerState(Player.PlayerState.LongJumping);
-        }
+      myRb2d.AddForce(new Vector2(0f, shortJumpForce));
+      isGrounded = false;
+      player.SetPlayerState(Player.PlayerState.ShortJumping);
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+  }
+
+  public void LongJumpCommand()
+  {
+    if (isGrounded && !GameManager.Instance.IsGamePaused)
     {
-        if (collision.transform.CompareTag("Sidewalk"))
-        {
-            canJump = true;
-            player.SetPlayerState(Player.PlayerState.Running);
-        }
+      myRb2d.AddForce(new Vector2(0f, longJumpForce));
+      isGrounded = false;
+      player.SetPlayerState(Player.PlayerState.LongJumping);
     }
+  }
+
+  public void DashCommand()
+  {
+    if (isGrounded && !GameManager.Instance.IsGamePaused)
+    {
+      player.SetPlayerState(Player.PlayerState.Dashing);
+      boxCollider2d_top.enabled = false;
+    }
+  }
+
+  private void OnCollisionEnter2D(Collision2D collision)
+  {
+    if (collision.transform.CompareTag("Sidewalk"))
+    {
+      isGrounded = true;
+      player.SetPlayerState(Player.PlayerState.Running);
+    }
+  }
+
+  public void ActivateTopBoxCollider2d()
+  {
+    boxCollider2d_top.enabled = true;
+  }
 }
